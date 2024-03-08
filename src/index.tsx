f37bc713df24395d1bdd88b7dee6a0eb1125dcea
Guilherme Gradaschi <guilhermegradas@gmail.com>
2024-03-08 19:18:19 -0300

    fix: mains index from css to scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './index.scss'
 import App from './App'
+import { AlertProvider } from './context'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <AlertProvider>
+    <App />
+  </AlertProvider>,
+)

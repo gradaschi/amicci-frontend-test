6d9cd1c19a6203f5fb029c7e08f98c0ef2feebab
Guilherme Gradaschi <guilhermegradas@gmail.com>
2024-03-08 19:17:06 -0300

    feat: enhanced app logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,43 +4,64 @@ import SearchBar from './components/Searchbar'
 import WeatherCard from './components/WeatherCard'
 import { getWeatherData, getCityLocation, getGeoLocation } from './services'
 import Parallax from './components/Parallax'
+import { useAlert } from './context'
 
 function App() {
-  const [location, setLocation] = React.useState<{
-    latitude: number
-    longitude: number
-  }>({
+  const [location, setLocation] = React.useState({
     latitude: 0,
     longitude: 0,
   })
-  const [weatherData, setWeatherData] = React.useState<any>(null)
+
+  const [weatherData, setWeatherData] = React.useState(null)
+  const showAlert = useAlert()
   const parallaxRef = useRef<any>(null)
 
-  const onSearch = (query: string) => {
-    getCityLocation(query).then((data) => {
-      setLocation(data)
-    })
+  const handleLocationError = () => {
+    showAlert('error', 'Erro ao buscar localização')
   }
 
-  const onGeoLocate = async () => {
-    getGeoLocation().then((data) => {
-      setLocation(data)
-    })
+  const handleWeatherDataError = () => {
+    showAlert('error', 'Erro ao buscar dados do clima')
+  }
+
+  const searchLocation = (query: string) => {
+    getCityLocation(query)
+      .then((data) => {
+        setLocation(data)
+        parallaxRef.current?.scrollTo(1)
+      })
+      .catch(handleLocationError)
+  }
+
+  const geoLocate = async (scroll: boolean) => {
+    getGeoLocation()
+      .then((data) => {
+        setLocation(data)
+        if (scroll) parallaxRef.current?.scrollTo(1)
+      })
+      .catch(handleLocationError)
   }
 
   useEffect(() => {
     if (location.latitude === 0 && location.longitude === 0) return
-    getWeatherData(location.latitude, location.longitude).then((data) => {
-      setWeatherData(data)
-    })
+
+    getWeatherData(location.latitude, location.longitude)
+      .then((data) => {
+        setWeatherData(data)
+      })
+      .catch(handleWeatherDataError)
   }, [location])
+
+  useEffect(() => {
+    geoLocate(false)
+  }, [])
 
   return (
     <div className='app'>
       <Parallax ref={parallaxRef}>
         <SearchBar
-          onSearch={onSearch}
-          onGeoLocate={onGeoLocate}
+          onSearch={searchLocation}
+          onGeoLocate={geoLocate}
         />
         <WeatherCard weatherData={weatherData} />
       </Parallax>
